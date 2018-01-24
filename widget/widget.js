392591bc928deb7a453c06ec422ac1cf9df49a98
Darren Barklie <darrenbarklie@gmail.com>
2018-01-24 22:35:29 +0000

    Fetch data as JSON

--- widget/widget.js
+++ widget/widget.js
@@ -6,3 +6,30 @@
  * @copyright Copyright (c) 2018 Interaction Design Foundation
  *
  */
+ 
+ const aside = document.querySelector('aside#widget-idf-ux');
+ const url = 'https://test.information-architecture.org/widgets/ux-daily-v2?ep=mads_soegaard';
+ 
+ //const url = 'https://randomuser.me/api/?results=10';
+ 
+ 
+ fetch(url)
+   // Transform data into JSON (WORKS)
+   .then((resp) => resp.json())
+ 
+    // Handle data
+   .then(function(data){
+    
+    
+    console.log(data)
+   })
+   // Handle error
+   .catch(function(error){
+     
+   });
+ 
+ 
+ 
+
+ console.log('End JS');
+ 
